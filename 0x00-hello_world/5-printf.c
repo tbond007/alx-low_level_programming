71319b38c0af6a6298a04e4311b494496610a172
yusuf mustapha <tbondfit@gmail.com>
2022-09-08 16:33:25 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,5 +6,5 @@
  *
  * Description - using the main function           * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0                                       */                                               int main(void)                                    {
-        puts("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);                               }
