061079638b7aec1296226f41345726f4111ce2b3
yusuf mustapha <tbondfit@gmail.com>
2022-09-14 02:05:49 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,6 @@
 
 /**
  * print_sign - print + if n is greater than zero, 0 if n is zero and - if n is less than zero.
- *
  * @n: takes integer type input for function.
  *
  * Return: 1 if +, 0 if 0 and -1 if -
