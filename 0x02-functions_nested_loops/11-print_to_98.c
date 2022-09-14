ebb52cfc91ba4084d61444ccd4ab7002e5367fb9
yusuf mustapha <tbondfit@gmail.com>
2022-09-14 02:58:45 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,13 +2,12 @@
 
 /**
  * print_to_98 - print n to 98 counts
- *           separated by comma, followed
- *           by space and number should be
- *           printed in order
+ * separated by comma, followed
+ * by space and number should be
+ * printed in order
  *
  * @n: input
 */
-
 void print_to_98(int n)
 {
 	int count;
