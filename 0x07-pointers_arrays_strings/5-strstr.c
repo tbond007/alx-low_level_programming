d55f59feb7dace6bc4b8e8e36b1e4a1db897ec46
yusuf mustapha <tbondfit@gmail.com>
2022-09-26 14:22:41 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 		while (needle[j] && (haystack[i] == needle[0]))
 		{
 			if (haystack[i + j] == needle[j])
-				j=+;
+				j=++;
 			else
 				break;
 		}
