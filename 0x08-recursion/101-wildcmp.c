6de3296186c93fe2f911eb35f0303971fca3a6ce
yusuf mustapha <tbondfit@gmail.com>
2022-09-28 12:32:52 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,7 +10,7 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0')
-	return (s1 == '\0');
+		return (s1 == '\0');
 	if (*s2 == *s1)
 		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
