7d21bc7bfff37d37ffe99188863d26ec35d61cbb
yusuf mustapha <tbondfit@gmail.com>
2022-10-06 11:50:29 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,6 +20,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		return (p);
 	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	else if (new_size == old_size)
 		return (ptr);
 
