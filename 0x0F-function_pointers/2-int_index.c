05390c91b90b926d6206529a8452fe10002ee308
yusuf mustapha <tbondfit@gmail.com>
2022-10-13 12:50:54 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,11 +3,12 @@
 
 /**
  * int_index - searches for an integer
- * @array: array to serch through
+ * @array: array to search through
  * @size: size of array
  * @cmp: function used to compare
  *
  * Return: first index for which cmp doesn't return 0, or -1
+ * if no match is found or size is negative
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
